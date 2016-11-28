18b412eeb732b069fe2e29aa0c08a4a25cda37bb
Jeremy Clark <clark.jm.72@gmail.com>
2016-11-28 14:52:09 -0500

    Added some networking support, client_send method will be used to send messages to the server. server.c will be a separate process altogether.

---
+++ client_send.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+ 
+void client_send(char *message)
+{
+    int socket_desc;
+    struct sockaddr_in server;
+     
+    //Create socket
+    if ((socket_desc = socket(AF_INET , SOCK_STREAM , 0)) == -1)
+    {
+		return 1;
+    }
+         
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 80 );
+ 
+    //Connect to remote server
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        return 1;
+    }
+     
+    //Send some data
+    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    {
+        return 1;
+    }     
+    return 0;
+}
